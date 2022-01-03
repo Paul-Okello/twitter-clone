105cbc8d1f9b965d42576e716a5dfe7f53b6cdda
Paul-Okello <paulokello273@gmail.com>
2022-01-03 16:31:04 +0300

    ✨ New Feature: Sidebar Component Ui

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,7 +1,44 @@
+import Image from "next/image";
+import SidebarLink from "./SidebarLink";
+import { HomeIcon } from "@heroicons/react/solid";
+import {
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ClipboardListIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+} from "@heroicons/react/outline";
+
 function Sidebar() {
   return (
-    <div>
-      <h1 className='text-white'>SideBar</h1>
+    <div className='hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full first-letter:'>
+      <div className='flex items-center justify-center w-14 h-14 hover-animation p-0 xl:ml-24'>
+        <Image src='https://rb.gy/ogau5a' width={30} height={30} alt='' />
+      </div>
+      <div className='space-y-2.5 mt-4 mb-2.5 xl:ml-24'>
+        <SidebarLink text='Home' Icon={HomeIcon} active />
+        <SidebarLink text='Explore' Icon={HashtagIcon} />
+        <SidebarLink text='Notifications' Icon={BellIcon} />
+        <SidebarLink text='Messages' Icon={InboxIcon} />
+        <SidebarLink text='Bookmarks' Icon={BookmarkIcon} />
+        <SidebarLink text='Lists' Icon={ClipboardListIcon} />
+        <SidebarLink text='Profile' Icon={UserIcon} />
+        <SidebarLink text='More' Icon={DotsCircleHorizontalIcon} />
+      </div>
+      <button className='hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]'>
+        Tweet
+      </button>
+      <div className='text-[#d9d9d9] flex items-center justify-center mt-auto hover-animation xl:ml-auto xl:-mr-5 mb-3'>
+        <img src='...' alt='' className='h-10 w-10 rounded-full xl:mr-2.5' />
+        <div className='hidden xl:inline leading-5'>
+          <h4 className='font-bold'>Lopoz</h4>
+          <p className='text-[#6e767d]'>@Lopoz</p>
+        </div>
+        <DotsHorizontalIcon className='hidden h-5 xl:inline ml-10' />
+      </div>
     </div>
   );
 }
