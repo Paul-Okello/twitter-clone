b67043ac2a46388093eaf0a401229f383efb6341
Paul-Okello <paulokello273@gmail.com>
2022-01-04 14:09:13 +0300

    👌 IMPROVE:  Bug fixes

--- firebase.js
+++ firebase.js
@@ -5,16 +5,26 @@ import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: `${process.env.apiKey}`,
-  authDomain: `${process.env.authDomain}`,
-  projectId: `${process.env.projectId}`,
-  storageBucket: `${process.env.storageBucket}`,
-  messagingSenderId: `${process.env.messagingSenderId}`,
-  appId: `${process.env.appId}`,
-  measurementId: `${process.env.measurementId}`,
+  apiKey: "AIzaSyD_ovuHFk72FUQ-InokFqa8Ga3XimVdaDo",
+  authDomain: "twitter-clone-002.firebaseapp.com",
+  projectId: "twitter-clone-002",
+  storageBucket: "twitter-clone-002.appspot.com",
+  messagingSenderId: "137329015865",
+  appId: "1:137329015865:web:885a23ea1d5627313ba1e8",
+  measurementId: "G-JL5JLBSGVB",
 };
+
+// const firebaseConfig = {
+//   apiKey: process.env.apiKey,
+//   authDomain: process.env.authDomain,
+//   projectId: process.env.projectId,
+//   storageBucket: process.env.storageBucket,
+//   messagingSenderId: process.env.messagingSenderId,
+//   appId: process.env.appId,
+//   measurementId: process.env.measurementId,
+// };
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
