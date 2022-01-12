29b0e7502421e0e5508985e506febd48279dd619
Paul-Okello <paulokello273@gmail.com>
2022-01-12 10:57:36 +0300

    💄 UI:  Added Global Loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import { RecoilRoot } from "recoil";
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const progress = new ProgressBar({
+  size: 4,
+  color: "#1d9bf0",
+  className: "z-50",
+  delay: 100,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+  return (
+    <SessionProvider session={session}>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </SessionProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
